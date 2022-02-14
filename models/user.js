17a9196528377bb0ef68fde70f5e04f60fe13f7e
pavle.vukovic <pavle.vukovic@itcentar.rs>
2022-02-14 11:52:23 +0100

    Add user model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+// imports
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: Number,
+    default: null,
+  },
+});
+
+// generate token
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, username: this.username },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+
+// hash password
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// compare password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// export
+module.exports = mongoose.model("User", userSchema);
