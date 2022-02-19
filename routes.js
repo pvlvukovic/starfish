8326e210d50e3a3d41e70ca9f51eb643cec66a79
pavle.vukovic <pavle.vukovic@itcentar.rs>
2022-02-19 15:52:03 +0100

    Remove post and comment, needs to be redone

--- routes.js
+++ routes.js
@@ -5,13 +5,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("./controllers/auth");
 const userController = require("./controllers/user");
-const postController = require("./controllers/post");
-const commentController = require("./controllers/comment");
 
 router.use("/auth", authController);
 router.use("/users", userController);
-router.use("/posts/:postId/comments", commentController);
-router.use("/posts", postController);
 
 // exports
 module.exports = router;
