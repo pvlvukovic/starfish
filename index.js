738072d770341a938c607bad12fac512ac3a486f
pavle.vukovic <pavle.vukovic@itcentar.rs>
2022-03-24 11:05:28 +0100

    fix client serving

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api", routes);
 
 // serve client folder
-app.use(express.static(path.join(__dirname, "client")));
-app.get("/", (req, res) => {
+app.use(express.static("client"));
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
